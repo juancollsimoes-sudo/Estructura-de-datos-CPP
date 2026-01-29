6206fd67c2d9f14ab46a10109e58cd3de44be21e
Juan Coll <juancollsimoes@gmail.com>
2026-01-29 09:06:46 -0400

    Implement linked list and node classes in Java

---
+++ Listas/Listas simplemente enlazadas.java
@@ -0,0 +1,93 @@
+public class Nodo <T> {
+    protected T data;
+    protected Nodo <T> pNext;
+    
+    
+    Nodo (T elem){
+        this.data = elem;
+        this.pNext = null;
+    }
+}
+
+public class Lista <T> {
+    private Nodo <T> pFirst;
+    private int iN;
+    private String name;
+    
+    public Lista (String s){ 
+        this.name = s;
+        this.pFirst = null;
+        this.iN = 0;
+    }
+    
+    public int Tamaño(){
+        return this.iN;
+    }
+    
+    public boolean EsVacio(){
+        return this.pFirst == null;
+    }
+    
+    public Nodo<T> Primero(){
+        return this.pFirst;
+    }
+    
+    public T Leer(Nodo<T> pValor){
+        return pValor.data;
+    }
+    
+    
+    public Nodo<T> Ultimo(){
+        return null;
+    }
+    public Nodo<T> Proximo(Nodo<T> pValor){
+        if (pValor != this.Ultimo()){
+            return pValor.pNext;
+        } else{
+            return this.Ultimo();
+        }
+    }
+            
+  public void Insertar (T x, Nodo<T> pValor){
+      Nodo<T> pNew= new Nodo<>(x);
+      if (this.EsVacio()){
+          this.pFirst = pNew;
+          this.iN++;
+      } else{
+          if (pValor != this.Ultimo()){
+              pNew.pNext = pValor.pNext;
+              pValor.pNext = pNew;
+              this.iN++;
+          }
+      }  
+  }   
+  
+  public Nodo<T> Final(){
+      if (this.EsVacio()){
+          return null;
+      } else{
+          Nodo<T> pAux = this.Primero();
+          while(pAux.pNext != this.Ultimo()){
+              pAux = this.Proximo(pAux);
+          }
+          return pAux;
+      }
+  }
+  
+  public String Recorrer(){
+      Nodo<T> pAux;
+      String cadena = "";
+      if (this.EsVacio()){
+          return "La Lista esta vacia";
+      } else{
+          pAux=this.Primero();
+          while(pAux!=this.Ultimo()){
+              cadena = cadena + this.Leer(pAux) + ". ";
+              pAux = this.Proximo(pAux);
+          }
+      }
+      return cadena;
+  }
+  
+    
+}
